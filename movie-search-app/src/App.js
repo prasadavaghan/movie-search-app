67b0193037c79226b82858b8ad9b262f2f52eab5
Prasad Avaghan <89061133+prasadavaghan@users.noreply.github.com>
2025-04-03 17:15:37 +0000

    Made separate route for movie

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -1,31 +1,14 @@
 import './App.css';
 import { useEffect, useState } from "react"
-
+import {BrowserRouter ,Routes ,Route} from "react-router-dom"
+import MovieSearch from './components/movies';
 function App() {
-  const [data, setData] = useState([]);
-  const [value, setValue] = useState("")
-  useEffect(() => {
-    fetch(` https://api.tvmaze.com/search/shows?q=${value}`)
-      .then((res) => res.json())
-      .then((res1) => setData(res1))
-      .catch((err) => { console.log("Error", err) })
-  }, [value])
-  console.log("Data", data)
   return (
-    <div className="App">
-      Movie Search
-      <input style={{display:"block" ,margin:"auto"}} type="text" placeholder="Write Movie name" value={value} onChange={(e) => setValue(e.target.value)} />
-      <ul>
-        {data?.map((item) =>
-          <li style={{ display: "flex" ,border:"1px solid black" ,justifyContent:"space-between",alignItems:"center"}} >
-            <a style={{marginLeft:"20px"}} href={item.show.officialSite ? item.show.officialSite : item.show.url} target='_blank' rel='noreferrer'>
-              <h6>{item.show.name}</h6>
-            </a>
-            <p style={{width:"50%" ,marginRight:"50px"}} dangerouslySetInnerHTML={{ __html: item.show.summary }} ></p>
-          </li>
-        )}
-      </ul>
-    </div>
+    <BrowserRouter className="App">
+      <Routes>
+        <Route path='/' element={<MovieSearch />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ movie-search-app/src/components/movies.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+
+const MovieSearch = () => {
+    const [data, setData] = useState([]);
+    const [value, setValue] = useState("")
+    useEffect(() => {
+        fetch(` https://api.tvmaze.com/search/shows?q=${value}`)
+            .then((res) => res.json())
+            .then((res1) => setData(res1))
+            .catch((err) => { console.log("Error", err) })
+    }, [value])
+    return (
+        <div>
+            Movie Search
+            <input style={{ display: "block", margin: "auto" }} type="text" placeholder="Write Movie name" value={value} onChange={(e) => setValue(e.target.value)} />
+            <ul>
+                {data?.map((item) =>
+                    <li style={{ display: "flex", border: "1px solid black", justifyContent: "space-between", alignItems: "center" }} >
+                        <a style={{ marginLeft: "20px" }} href={item.show.officialSite ? item.show.officialSite : item.show.url} target='_blank' rel='noreferrer'>
+                            <h6>{item.show.name}</h6>
+                        </a>
+                        <p style={{ width: "50%", marginRight: "50px" }} dangerouslySetInnerHTML={{ __html: item.show.summary }} ></p>
+                    </li>
+                )}
+            </ul>
+        </div>
+    )
+}
+
+export default MovieSearch;
